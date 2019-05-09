2a44b63fb3a9bcea957b1354fa1d2392eb08bc1f
Gintaras <gintaras.garsva@gmail.com>
2019-05-09 14:58:25 +0300

    hack #2 v2

--- index.php
+++ index.php
@@ -1,59 +1,18 @@
 <?php
 
-$greeting_message = rand(1, 3);
-if ($greeting_message == 1) {
-    $greeting = 'Aloha!!!';
-}
-if ($greeting_message == 2) {
-    $greeting = 'Supp Hommies!!!';
-}
-if ($greeting_message == 3) {
-    $greeting = 'What\'s crackin??';
-}
+$hack = filter_input_array(INPUT_POST, [
+    'hack_me' => FILTER_SANITIZE_SPECIAL_CHARS,
+        ]);
 
 ?>
-<!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="refresh" content="10" >
-        <style>
-            .background{
-                background-image: url('https://st4.depositphotos.com/9568760/20501/i/1600/depositphotos_205017032-stock-photo-white-fractured-surface-abstract-background.jpg');
-                background-size:cover;
-            }
-            .text{
-                text-align: center;
-                color: greenyellow;
-            }
-            nav{
-                text-align:center;
-                padding-bottom: 200px;
-            }
-            nav a{
-                border: 1px solid green;
-                padding: 10px;
-                background-color: green;
-                text-decoration: none;
-                color: white;
-            }
-            h1{
-                font-size: 100px;
-            }
-            h2{
-                font-size: 70px;
-            }
-        </style>
+        <title>hack</title>
     </head>
-    <body class="background">
-        <nav>
-            <a href="index.php">HOME</a> 
-            <a href="about.php">ABOUT</a> 
-            <a href="pz2a-mat.php">PZ2A-MAT</a> 
-            <a href="contact.php">CONTACT</a> 
-        </nav>
-        <div class="text">
-            <h1>GRAY 83</h1>
-            <h2><?php print $greeting; ?></h2>
-        </div>
+    <body>
+        <form method="post">
+            <input type="text" name="hack_me" placeholder="Hack Me" required>
+            <button name="button" value="push">Try</button>
+        </form>
     </body>
 </html>
