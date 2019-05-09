08cced00b5386909b49f2b406bc8cc0ad2988002
Gintaras <gintaras.garsva@gmail.com>
2019-05-09 17:00:35 +0300

    uzduotis #3

--- index.php
+++ index.php
@@ -1,22 +1,63 @@
 <?php
 
-$hack = filter_input_array(INPUT_POST, [
-    'hack_me' => FILTER_SANITIZE_SPECIAL_CHARS,
-        ]);
-
-var_dump($hack);
-$input = $hack['hack_me'];
+$form = [
+    'fields' => [
+        'mano_vardas' => [
+            'label' => 'Vardas',
+            'type' => 'text',
+            'placeholder' => 'Vardas',
+        ],
+        'kiek_zirniu_turiu' => [
+            'label' => 'Zirniai',
+            'type' => 'text',
+            'placeholder' => '1-100',
+        ],
+        'paslaptis_kodel_turiu_zirniu' => [
+            'label' => 'Reason',
+            'type' => 'password',
+            'placeholder' => 'Issipasakok',
+        ],
+    ],
+    'buttons' => [
+        'do_zirniai' => [
+            'value' => 'do_zirniai',
+            'label' => 'paberti...',
+        ],
+        'do_grikiai' => [
+            'value' => 'do_grikiai',
+            'label' => 'paberti...',
+        ],
+    ],
+];
 
 ?>
 <html>
     <head>
-        <title>hack</title>
+        <title>uzduotis #3</title>
+        <style>
+            .inputs{
+                display:block;
+                margin-top:1%;
+                width: 100px;
+            }
+            button{
+                margin-top:1%;
+            }
+        </style>
     </head>
     <body>
-        <h1><?php print $input; ?></h1>
-        <form method="post">
-            <input type="text" name="hack_me" placeholder="Hack Me" required="">
-            <button name="button" value="push">Try</button>
+        <form method="POST">
+            <?php foreach ($form['fields'] as $field_id => $field): ?>
+                <label class="inputs">
+                    <span><?php print $field['label']; ?></span>
+                    <input type="<?php print $field['type']; ?>" name="<?php print $field['label']; ?>" placeholder="<?php print $field['placeholder']; ?>" >
+                </label>
+            <?php endforeach; ?>
+            <?php foreach ($form['buttons'] as $button_id => $button): ?>
+                <button name="action" value="<?php print $button['value']; ?>">
+                    <?php print $button['label']; ?>
+                </button>
+            <?php endforeach; ?>
         </form>
     </body>
 </html>
